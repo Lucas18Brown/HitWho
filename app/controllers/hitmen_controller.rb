96812e8298bf829015375a124a643ac54091a8b2
Tomas Bui Lai <tomass123ss@gmail.com>
2022-11-21 17:13:32 +0000

    delete feature

--- app/controllers/hitmen_controller.rb
+++ app/controllers/hitmen_controller.rb
@@ -1,2 +1,10 @@
 class HitmenController < ApplicationController
+
+  def destroy
+    @hitman = Hitman.find(params[:id])
+    @hitman.destroy
+    redirect_to hitmen_path,
+    status: :see_other
+  end
+
 end
