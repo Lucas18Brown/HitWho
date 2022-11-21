413c6dceaa23f61d22861a94f0c942f462051ef1
lucas brown <lucas20brown@hotmail.co.uk>
2022-11-21 17:18:53 +0000

    added index and show to hitmen controller

--- app/controllers/hitmen_controller.rb
+++ app/controllers/hitmen_controller.rb
@@ -1,2 +1,15 @@
 class HitmenController < ApplicationController
+  before_action :set_hitman, only: %i[show]
+  def index
+    @hitmen = Hitman.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_hitman
+    @hitman = Hitman.find(params[:id])
+  end
 end
