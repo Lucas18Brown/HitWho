ea83c35d8e915e59c23423f1c8f8c3520ea12ab6
juan mendoza <j.mendoza712@gmail.com>
2022-11-21 16:22:11 +0000

    User created using Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Heroku server config for production
+  config.action_mailer.default_url_options = { host: 'https://airbnb-lucas18brown.herokuapp.com' }
+
 end
