b867d2acda00afc86eadacfa730985a5a26fa167
Ali-Hamza Asam <asamalihamza@gmail.com>
2022-11-22 10:46:11 +0000

    Added model and controller for booking with correct foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_163346) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_104325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.bigint "hitmen_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hitmen_id"], name: "index_bookings_on_hitmen_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "hitmen", force: :cascade do |t|
     t.string "name"
@@ -38,5 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_163346) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "hitmen", column: "hitmen_id"
+  add_foreign_key "bookings", "users"
   add_foreign_key "hitmen", "users"
 end
