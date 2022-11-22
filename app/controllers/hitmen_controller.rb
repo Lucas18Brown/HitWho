3bc3afebc074c3fab6dc82d72e744c2a913c99e2
juan mendoza <j.mendoza712@gmail.com>
2022-11-22 10:11:59 +0000

    Hitmen edit method

--- app/controllers/hitmen_controller.rb
+++ app/controllers/hitmen_controller.rb
@@ -1,2 +1,14 @@
 class HitmenController < ApplicationController
+
+  def edit
+    # searching instance of Hitman through its id
+    @hitman = Hitman.find(params[:id])
+  end
+
+  def update
+    @hitman = Hitman.find(hitmen_params)
+    @hitman.update(@hitman)
+    # Redirect to hitman's profile
+    redirect_to hitman_path(@hitman)
+  end
 end
