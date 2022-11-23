a34241a78e43e1c79e1d22f3cd2dbf799004415e
Ali-Hamza Asam <asamalihamza@gmail.com>
2022-11-23 11:50:47 +0000

    Added destroy method to bookings, and changed booking destroy route.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
   before_action :set_hitman, only: %i[index new create]
+  before_action :set_booking, only: %i[destroy]
 
   def show_bookings
     @bookings = Booking.where(user: current_user)
@@ -20,7 +21,17 @@ class BookingsController < ApplicationController
     end
   end
 
+  def destroy
+    @booking.destroy
+
+    redirect_to user_path(current_user), status: :see_other
+  end
+
   private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 
   def set_hitman
     @hitman = Hitman.find(params[:hitman_id])
