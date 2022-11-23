9be52f5948a3f1e0319bd3dfa9b52465fdde7b49
Ali-Hamza Asam <asamalihamza@gmail.com>
2022-11-23 14:28:33 +0000

    Added dependet destroy to bookings

--- app/models/hitman.rb
+++ app/models/hitman.rb
@@ -1,5 +1,6 @@
 class Hitman < ApplicationRecord
   METHOD = %w[gun punch car superpower knife fatality poison]
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
+
 end
