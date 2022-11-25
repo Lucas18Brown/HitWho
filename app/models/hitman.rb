4fa9780e5f143d401befd98477feef8c0133eb18
Ali-Hamza Asam <asamalihamza@gmail.com>
2022-11-25 11:54:48 +0000

    Fixed depenencies and index html

--- app/models/hitman.rb
+++ app/models/hitman.rb
@@ -1,7 +1,7 @@
 class Hitman < ApplicationRecord
   METHOD = %w[gun punch car superpower knife fatality poison]
   belongs_to :user
-  
+
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings, dependent: :destroy
   has_one_attached :photo
