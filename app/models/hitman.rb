abb28f1a410f5ac4afc990d4d1561bc42d6928d4
Ali-Hamza Asam <asamalihamza@gmail.com>
2022-11-25 12:38:25 +0000

    Added form validations and error messages

--- app/models/hitman.rb
+++ app/models/hitman.rb
@@ -1,8 +1,12 @@
 class Hitman < ApplicationRecord
   METHOD = %w[gun punch car superpower knife fatality poison]
   belongs_to :user
-
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings, dependent: :destroy
   has_one_attached :photo
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
+
 end
